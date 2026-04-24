c201c8478f29dc97891701535ddf12b9a9fa1e49
camilo <cacb2908@gmail.com>
2026-04-23 23:45:47 -0500

    commit

---
+++ pagerestaurant/script.js
@@ -0,0 +1,16 @@
+
+let posicion = 0;
+
+function moverChef(direccion) {
+    const track = document.getElementById('chefsTrack');
+    const tarjetas = track.querySelectorAll('.tarjeta');
+    const total = tarjetas.length;
+    const ancho = tarjetas[0].offsetWidth + 40; // 40 = gap
+
+    posicion += direccion;
+
+    if (posicion < 0) posicion = total - 1;
+    if (posicion >= total) posicion = 0;
+
+    track.style.transform = `translateX(-${posicion * ancho}px)`;
+}
